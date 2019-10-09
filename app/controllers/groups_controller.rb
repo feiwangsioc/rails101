e43dd60f6515d6e802e1408bc1d4bd4d232cbdf6
fei wang <feiwangsioc@gmail.com>
2019-10-09 01:39:14 +0000

    ch04

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,19 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   
   def index
     @groups = Group.all
   end 
   
   def show
-    @group = Group.find(params[:id])
+    
+    find_group_and_check_permission
+
   end 
   
   def edit
-    @group = Group.find(params[:id])
+    
   end
   
   def new 
@@ -19,6 +22,7 @@ class GroupsController < ApplicationController
   
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
     
     if @group.save
       
@@ -29,7 +33,10 @@ class GroupsController < ApplicationController
   end 
   
   def update
-    @group = Group.find(params[:id])
+    
+   
+
+    
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -38,13 +45,21 @@ class GroupsController < ApplicationController
   end 
   
   def destroy
-    @group = Group.find(params[:id])
+    
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
   end 
 
   private
+  
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
   
   def group_params
     params.require(:group).permit(:title, :description)
