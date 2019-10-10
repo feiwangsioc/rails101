ad4b449f497004d0f478d3edda5fa10bf7c43a62
fei wang <feiwangsioc@gmail.com>
2019-10-10 00:49:41 +0000

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
          
   has_many :gruops
   has_many :posts
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships, :source => :group 
 end
